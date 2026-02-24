0ade0b8ea29cbe59b1fa9c1b78c764b234d7a36d
alazzaro <alfio.lazzaro@gmail.com>
2026-02-24 10:10:12 +0200

    Update to cray-mpich 9

--- sourceme_craympi.sh
+++ sourceme_craympi.sh
@@ -1,7 +1,12 @@
+
+export PE_LD_LIBRARY_PATH=system # Force update of the LD_LIBRARY_PATH, instead of CRAY_LD_LIBRARY_PATH
+
 module load PrgEnv-gnu
 module load libfabric
+
 case "$USER" in
     lazzaroa)
+	module load cray-mpich/9.0.1
 	module swap craype-x86-rome craype-x86-trento
 	module load craype-accel-amd-gfx90a
 	module load rocm
