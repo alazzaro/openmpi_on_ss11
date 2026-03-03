6edb786403a8764c1dfdd92a102bdae1d42fb5fa
alazzaro <alfio.lazzaro@gmail.com>
2026-03-03 14:24:07 +0200

    commit OpenMPI verbose affinity test

--- affinity/run_ompi.sh
+++ affinity/run_ompi.sh
@@ -18,9 +18,9 @@ export OMPI_MCA_smsc=xpmem
 APP=xthi
 
 # Take all GPUs
-RUN_COMMAND="mpirun --bind-to core --map-by L3cache:pe=${OMP_NUM_THREADS} --mca hwloc_base_binding_policy core --mca hwloc_base_mem_bind policy:bind ../select_gpu.sh"
+RUN_COMMAND="mpirun --bind-to core --map-by L3cache:pe=${OMP_NUM_THREADS} --mca hwloc_base_binding_policy core --mca hwloc_base_mem_bind policy:bind --report-bindings ../select_gpu.sh"
 # Take only 2 GPUs
-#RUN_COMMAND="mpirun --bind-to numa --map-by numa --mca hwloc_base_binding_policy core --mca hwloc_base_mem_bind policy:bind ../select_gpu.sh"
+#RUN_COMMAND="mpirun --bind-to numa --map-by numa --mca hwloc_base_binding_policy core --mca hwloc_base_mem_bind policy:bind --report-bindings ../select_gpu.sh"
 
 mpicc -g -fopenmp -o ${APP}_ompi.c.x ${APP}.c
 ${RUN_COMMAND} ./${APP}_ompi.c.x | sort -n -k 4 -k 6
